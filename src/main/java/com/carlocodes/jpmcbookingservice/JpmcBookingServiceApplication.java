3735f6cc291368b7dadca642b510d83a557e4799
carlocodes <Gian Uy>
2024-02-07 19:52:56 +0800

    initial commit

---
+++ src/main/java/com/carlocodes/jpmcbookingservice/JpmcBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carlocodes.jpmcbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpmcBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpmcBookingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carlocodes/jpmcbookingservice/JpmcBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlocodes.jpmcbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpmcBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
